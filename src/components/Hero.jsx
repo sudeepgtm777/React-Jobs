a4f7916164176ec490f3dff07359cabd70e664ad
Sudeep Gautam <sudeepgautam@Sudeeps-MacBook-Air.local>
2025-09-05 17:10:36 +0545

    Configuring files and adding it

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hero = (props) => {
+  return (
+    <section class="bg-indigo-700 py-20 mb-4">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+        <div class="text-center">
+          <h1 class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+            {props.title}
+          </h1>
+          <p class="my-4 text-xl text-white">{props.subtitle}</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
