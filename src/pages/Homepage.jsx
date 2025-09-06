90fb6d64f0ebad3731f56ab03bdf1883ede370b4
Sudeep Gautam <sudeepgautam@Sudeeps-MacBook-Air.local>
2025-09-06 15:14:56 +0545

    Adding Seperate page

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+const Homepage = () => {
+  return (
+    <>
+      <Hero title="Hero" subtitle="The place is awesome" />
+    </>
+  );
+};
+
+export default Homepage;
