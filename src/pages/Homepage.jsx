d269ba44beeb19adee0ef67a64e6dd6a92bc84bb
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-09-08 13:55:52 +0545

    Edit in title

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import Hero from "../components/Hero";
-import HomeCards from "../components/HomeCards";
-import JobsListing from "../components/JobsListing";
-import ViweAllJobs from "../components/ViewAllJobs";
+import React from 'react';
+import Hero from '../components/Hero';
+import HomeCards from '../components/HomeCards';
+import JobsListing from '../components/JobsListing';
+import ViweAllJobs from '../components/ViewAllJobs';
 
 const Homepage = () => {
   return (
     <>
-      <Hero title="Hero" subtitle="The place is awesome" />
+      <Hero
+        title='React Jobs'
+        subtitle='The place for developers to find job'
+      />
       <HomeCards />
       <JobsListing isHome={true} />
       <ViweAllJobs />
