abcdc495fa45d6429e6cd63eec00b781b1185765
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-09-08 13:51:48 +0545

    Edit for job

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 const App = () => {
   // Add new Job
@@ -25,10 +26,21 @@ const App = () => {
     return;
   };
 
-  // Delete a job
+  // Delete a Job
   const deleteJob = async (id) => {
     const res = await fetch(`/api/jobs/${id}`, {
       method: 'DELETE',
+    });
+    return;
+  };
+  // Update a Job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(job),
     });
     return;
   };
@@ -39,6 +51,11 @@ const App = () => {
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route
+          path='/edit-job/:id'
+          element={<EditJobPage updateJobSubmit={updateJob} />}
+          loader={jobLoader}
+        />
         <Route
           path='/jobs/:id'
           element={<JobPage deleteJob={deleteJob} />}
